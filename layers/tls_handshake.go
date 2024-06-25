0e3af2de8bc2dc8eda9d2e16979cf9f5971246d3
Peter Wang <wpj@datasec.online>
2024-06-25 19:40:03 +0800

    tls握手协议解析

--- layers/tls_handshake.go
+++ layers/tls_handshake.go
@@ -7,21 +7,61 @@
 package layers
 
 import (
+	"errors"
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers/tls"
+	"golang.org/x/crypto/cryptobyte"
+)
+
+type TLSHandshakeType uint8
+
+const (
+	TLSHandshakeClientHello        TLSHandshakeType = 1
+	TLSHandshakeServerHello        TLSHandshakeType = 2
+	TLSHandshakeCertificate        TLSHandshakeType = 11
+	TLSHandshakeServerKeyExchange  TLSHandshakeType = 12
+	TLSHandshakeCertificateRequest TLSHandshakeType = 13
+	TLSHandshakeServerHelloDone    TLSHandshakeType = 14
+	TLSHandshakeCertificateVerify  TLSHandshakeType = 15
+	TLSHandshakeClientKeyExchange  TLSHandshakeType = 16
+	TLSHandshakeUnknown            TLSHandshakeType = 255
 )
 
 // TLSHandshakeRecord defines the structure of a Handshare Record
 type TLSHandshakeRecord struct {
 	TLSRecordHeader
-	Raw []byte
+	HandshakeType TLSHandshakeType
+	ClientHello   *tls.ClientHelloInfo `json:"client_hello,omitempty"`
 }
 
 // DecodeFromBytes decodes the slice into the TLS struct.
 func (t *TLSHandshakeRecord) decodeFromBytes(h TLSRecordHeader, data []byte, df gopacket.DecodeFeedback) error {
 	// TLS Record Header
 	t.TLSRecordHeader = h
-	t.Raw = data
-	// TODO
 
+	if len(data) < 4 {
+		df.SetTruncated()
+		return errors.New("TLS Change Cipher Spec record incorrect length")
+	}
+
+	t.HandshakeType = TLSHandshakeType(data[0])
+	str := cryptobyte.String(data[1:])
+	var hs_len uint32
+	str.ReadUint24(&hs_len)
+	if hs_len+4 != uint32(t.Length) {
+		df.SetTruncated()
+		return errors.New("TLS handshake length mismatch")
+	}
+	switch t.HandshakeType {
+	case TLSHandshakeClientHello:
+		t.ClientHello = tls.UnmarshalClientHello(str)
+	case TLSHandshakeServerHello:
+	case TLSHandshakeCertificate:
+	case TLSHandshakeServerKeyExchange:
+	case TLSHandshakeCertificateRequest:
+	case TLSHandshakeServerHelloDone:
+	case TLSHandshakeCertificateVerify:
+	case TLSHandshakeClientKeyExchange:
+	}
 	return nil
 }
